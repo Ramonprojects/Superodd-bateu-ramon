ec2ed96fb5ff8be87da8d247bc5813a2625eb773
Ramonprojects <adm@ramonmago.com>
2026-05-12 18:24:14 -0300

    Add redirect handler in api/index.js

---
+++ api/index.js
@@ -0,0 +1,4 @@
+  export default function handler(req, res) {                                                                                                                                              
+    res.writeHead(302, { Location: 'https://rsob.sugestao.cc/' });
+    res.end();                                                                                                                                                                             
+  }

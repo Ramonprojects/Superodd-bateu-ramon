bb1b15fb1460d322a630e81c1bbb451f59d8ef1c
Ramonprojects <adm@ramonmago.com>
2026-05-12 19:21:00 -0300

    Update redirect URL in API handler

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
   export default function handler(req, res) {                                                                                                                                              
-    res.writeHead(302, { Location: 'https://rsob.sugestao.cc/' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=NF4J2KXLUFC&afp=so&home=1' });
     res.end();                                                                                                                                                                             
   }

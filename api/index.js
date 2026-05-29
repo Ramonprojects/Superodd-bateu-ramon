3ae1a1750798b982d318d3ed81b6e60cbb4f3461
Ramonprojects <adm@ramonmago.com>
2026-05-29 18:13:21 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=KQCYY8DJCH5&afp=so&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=P0U9TIF0CCV&afp3=so1&home=1' });
     res.end();
   }

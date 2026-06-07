c02d1539a286926bbacc86a22dc0c20150ebee48
Ramonprojects <adm@ramonmago.com>
2026-06-07 11:32:55 -0300

    chore: shareCode A05BYJDF5GE -> NXBMWTZGLB0 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=A05BYJDF5GE&afp3=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=NXBMWTZGLB0&afp3=so1&home=1' });
     res.end();
   }

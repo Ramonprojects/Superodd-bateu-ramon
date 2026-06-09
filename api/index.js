b914a88f713175a9cad12aa64a198d9b6b140e52
Ramonprojects <adm@ramonmago.com>
2026-06-09 10:39:24 -0300

    chore: shareCode MJ5DBE7AWEK -> M60VKS8W73S (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=MJ5DBE7AWEK&afp3=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=M60VKS8W73S&afp3=so1&home=1' });
     res.end();
   }

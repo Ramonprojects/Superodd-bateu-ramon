ccb57bd4a9b7672843b35db4319282d29e72b72e
Ramonprojects <adm@ramonmago.com>
2026-06-15 14:40:01 -0300

    chore: shareCode 9A0ESI1KSXV -> U71RGYNYIR3 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=9A0ESI1KSXV&afp2=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=U71RGYNYIR3&afp2=so1&home=1' });
     res.end();
   }

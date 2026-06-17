7b912a6d5f0b282e99e913104e9b978f5d258f02
Ramonprojects <adm@ramonmago.com>
2026-06-17 11:29:30 -0300

    chore: shareCode 8B1RFTFRY1F -> BPDWM1FW0HJ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=8B1RFTFRY1F&afp2=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=BPDWM1FW0HJ&afp2=so1&home=1' });
     res.end();
   }

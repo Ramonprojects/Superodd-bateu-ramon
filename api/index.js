a1320a0779699ebb2ea41e31143b6536c0ba8836
Ramonprojects <adm@ramonmago.com>
2026-06-18 21:33:19 -0300

    chore: shareCode MHNWAFOWASI -> J3CLE4WB5KL (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=MHNWAFOWASI&afp2=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=J3CLE4WB5KL&afp2=so1&home=1' });
     res.end();
   }

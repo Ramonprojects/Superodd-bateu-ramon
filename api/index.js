8125642605d39067d940e6e7a89f3c69e1a1e112
Ramonprojects <adm@ramonmago.com>
2026-06-20 12:40:32 -0300

    chore: shareCode D9SL0XOTRGY -> JTB9NM4YPJC (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=D9SL0XOTRGY&afp2=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=JTB9NM4YPJC&afp2=so1&home=1' });
     res.end();
   }

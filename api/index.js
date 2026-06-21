3bc81966f082a82c1becd391a4b3009df7e8fcaf
Ramonprojects <adm@ramonmago.com>
2026-06-21 20:37:22 -0300

    chore: shareCode TOC0TJ6F9LH -> 2E7UYJD3AD1 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=TOC0TJ6F9LH&afp2=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=2E7UYJD3AD1&afp2=so1&home=1' });
     res.end();
   }

55fbbaca9790bfda1201ae6d9f14f5a00ddeb235
Ramonprojects <adm@ramonmago.com>
2026-06-23 11:30:46 -0300

    chore: shareCode 2E7UYJD3AD1 -> UOUJK9I2U2P (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=2E7UYJD3AD1&afp2=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=UOUJK9I2U2P&afp2=so1&home=1' });
     res.end();
   }

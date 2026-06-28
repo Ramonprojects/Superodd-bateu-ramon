cfa7ff3620ecafeab241d22438b7bf2d59c52d93
Ramonprojects <adm@ramonmago.com>
2026-06-28 12:31:30 -0300

    chore: shareCode 4DTIBNSY215 -> S1NDYI8CMGF (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=4DTIBNSY215&afp2=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=S1NDYI8CMGF&afp2=so1&home=1' });
     res.end();
   }

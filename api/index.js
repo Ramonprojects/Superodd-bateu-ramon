75fa6bfded40e214e3ba50b2ddf6332e06c3b614
Ramonprojects <adm@ramonmago.com>
2026-06-29 11:13:48 -0300

    chore: shareCode S1NDYI8CMGF -> A9VE2T74RU7 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=S1NDYI8CMGF&afp2=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=A9VE2T74RU7&afp2=so1&home=1' });
     res.end();
   }

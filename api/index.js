9b6fbb91c97ce41458b36f2f80edc46edeb29181
Ramonprojects <adm@ramonmago.com>
2026-07-01 12:54:26 -0300

    chore: shareCode 68W3B7C62OJ -> 7H3UUII72WB (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=68W3B7C62OJ&afp2=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=7H3UUII72WB&afp2=so1&home=1' });
     res.end();
   }

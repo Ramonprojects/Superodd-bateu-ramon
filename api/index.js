cbc1ddb114e2f772f226b775d4c0d9bce6dfb6df
Ramonprojects <adm@ramonmago.com>
2026-07-05 09:22:12 -0300

    chore: shareCode 6ZD7W7MU8N0 -> LXA3H6AE0WK (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=6ZD7W7MU8N0&afp2=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=LXA3H6AE0WK&afp2=so1&home=1' });
     res.end();
   }

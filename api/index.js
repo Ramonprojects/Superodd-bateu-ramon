d98ff903096ce55d7fe75fffb91c368c30a90309
Ramonprojects <adm@ramonmago.com>
2026-07-06 10:37:24 -0300

    chore: shareCode LXA3H6AE0WK -> UAUDZJTQPZ9 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=LXA3H6AE0WK&afp2=so1&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=UAUDZJTQPZ9&afp2=so1&home=1' });
     res.end();
   }
